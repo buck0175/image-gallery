bea0e2ac3b862757be2aa30c28ac5b32f1010d60
Ben Buck <benbuck@Bens-MacBook-Pro-2.local>
2019-04-10 13:20:44 -0400

    add image responsiveness

--- main.js
+++ main.js
@@ -8,41 +8,102 @@ const $imageGrid = document.querySelector('.image-grid');
 // Array holding our image objects
 const images = [
   {
-    image: "images/1.png",
+    image: "https://source.unsplash.com/random/800x800",
+    alt: "Random Image"
   },
   {
-    image: "images/2.png",
+    image: "https://source.unsplash.com/random/900x800",
+    alt: "Random Image"
   },
   {
-    image: "images/3.png",
+    image: "https://source.unsplash.com/random/940x800",
+    alt: "Random Image"
   },
   {
-    image: "images/4.png",
+    image: "https://source.unsplash.com/random/930x800",
+    alt: "Random Image"
   },
   {
-    image: "images/5.jpeg",
+    image: "https://source.unsplash.com/random/920x800",
+    alt: "Random Image"
   },
   {
-    image: 'images/6.jpeg',
+    image: "https://source.unsplash.com/random/910x800",
+    alt: "Random Image"
   },
   {
-    image: "images/7.jpeg",
+    image: "https://source.unsplash.com/random/960x800",
+    alt: "Random Image"
   },
   {
-    image: "images/8.jpeg",
+    image: "https://source.unsplash.com/random/970x800",
+    alt: "Random Image"
   },
   {
-    image: "images/9.jpeg",
+    image: "https://source.unsplash.com/random/980x800",
+    alt: "Random Image"
   },
   {
-    image: "images/10.jpeg",
+    image: "https://source.unsplash.com/random/990x800",
+    alt: "Random Image"
   },
   {
-    image: "images/11.jpeg",
+    image: "https://source.unsplash.com/random/950x810",
+    alt: "Random Image"
   },
   {
-    image: "images/12.jpeg",
-  }
+    image: "https://source.unsplash.com/random/950x820",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x830",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x840",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x850",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x860",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x870",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x880",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x890",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x900",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x910",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x920",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x930",
+    alt: "Random Image"
+  },
+  {
+    image: "https://source.unsplash.com/random/950x940",
+    alt: "Random Image"
+  },
+
 ];
 
 
@@ -53,7 +114,7 @@ const imagehtml = [];
 // pushes images into imagehtml array
 for(let i = 0; i < images.length; i++){
   imagehtml.push(`<div class="image">`);
-  imagehtml.push(`<img class="image-link cover" src="${images[i].image}" alt="">`);
+  imagehtml.push(`<img class="image-link cover" src="${images[i].image}" alt="${images[i].alt}">`);
   imagehtml.push(`</div>`);
 }
 
@@ -82,9 +143,10 @@ const $modal = document.querySelector('.modal');
 $imageGrid.addEventListener('click', function(event){
   const $item = event.target.closest('.image');
   const $src = event.target.closest('img').src
+  const $alt = event.target.closest('img').alt
 
   if($item) {
-    $modal.style.display= "block";
+    $modal.style.display= "flex";
     const $modalHtml = [];
 
     $modalHtml.push(`<div class="modal-content">`);
